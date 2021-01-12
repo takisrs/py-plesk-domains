f7e8bf97c13df0165529d7a94d9c36a619436773
Panagiotis Pantazopoulos <takispadaz@gmail.com>
2021-01-12 22:12:32 +0200

    create setup script

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='py-plesk-domains',
+    version='1.0.0',
+    scripts=['py_plesk_domains.py'],
+    entry_points={
+        'console_scripts': [
+            'py-plesk-domains=py_plesk_domains:main',
+        ]
+    },
+    install_requires=[
+        'tabulate',
+        'pyOpenSSL',
+        'requests'
+    ]
+)
