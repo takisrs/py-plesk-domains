ca238d38ab1729ab562bb477059375fd726c819e
Panagiotis Pantazopoulos <takispadaz@gmail.com>
2021-01-16 18:17:12 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='py-plesk-domains',
-    version='1.1.0',
+    version='1.1.1',
     author="Panagiotis Pantazopoulos",
     author_email="takispadaz@gmail.com",
     description="Get some basic data about domains from a server with plesk panel",
