84a073e75f582ab84b377ec634be00c29713809d
Panagiotis Pantazopoulos <takispadaz@gmail.com>
2021-01-16 18:05:36 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='py-plesk-domains',
     version='1.1.0',
+    author="Panagiotis Pantazopoulos",
+    author_email="takispadaz@gmail.com",
+    description="Get some basic data about domains from a server with plesk panel",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/takisrs/py-plesk-domains",
     scripts=['py_plesk_domains.py'],
     entry_points={
         'console_scripts': [
             'py-plesk-domains=py_plesk_domains:main',
         ]
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[
         'tabulate',
         'pyOpenSSL',
